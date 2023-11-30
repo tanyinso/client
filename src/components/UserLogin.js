27bc7a6229d2d0add5b1dff8999b82b3bd079c9e
tanyinso <tanyinsobright237@gmail.com>
2023-11-29 21:39:30 -0500

    updated

--- src/components/UserLogin.js
+++ src/components/UserLogin.js
@@ -1,61 +1,59 @@
-import React from 'react'
-import { useState } from 'react'
-import axios from 'axios'
-import { useNavigate } from 'react-router-dom'
-import Cookies from 'universal-cookie'
+import React from 'react';
+import { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import Cookies from 'universal-cookie';
 
 const UserLogin = () => {
-     const cookie = new Cookies()
-     const [email, setEmail] = useState("");
-     const [password, setPassword] = useState("");
-     const [isLogedin, setIsLogedin] = useState(false);
-     const [message, setMessage] = useState("");
-     const location = useNavigate();
+  const cookie = new Cookies();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLogedin, setIsLogedin] = useState(false);
+  const [message, setMessage] = useState("");
+  const location = useNavigate();
 
-     const handleLogin = async (e) => {
-          e.preventDefault();
-          const UserLogin = new FormData();
-          UserLogin.append('email', email);
-          UserLogin.append('password', password)
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    const UserLogin = new FormData();
+    UserLogin.append('email', email);
+    UserLogin.append('password', password);
 
-          try {
-               const response = await axios({
-                    method: 'post',
-                    url: 'http://localhost:8888/user/login',
-                    data: UserLogin,
-                    headers: {
-                         'Content-Type': 'application/json'
-                    }
-               });
+    try {
+      const response = await axios({
+        method: 'post',
+        url: 'http://localhost:8888/user/login',
+        data: UserLogin,
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
 
-               setMessage(response.data.message)
-               cookie.set('TOKEN', response.data.token, {
-                    path: '/'
-               });
-               location('/');
-               window.location.reload()
-          } catch (error) {
-               console.log(error);
-               setIsLogedin(true)
-               setMessage(response.data.message)
-          }
+      setMessage(response.data.message);
+      cookie.set('TOKEN', response.data.token, {
+        path: '/'
+      });
+      location('/');
+      window.location.reload();
+    } catch (error) {
+      console.log(error);
+      setIsLogedin(true);
+      setMessage(error.message);
+    }
+  };
 
-     }
-     return (
-          <section className="form-container">
-               <form onSubmit={handleLogin}>
-                    <h3>login now</h3>
-                    <p>your email <span>*</span></p>
-                    <input type="email" placeholder="enter your email" required maxLength="50" className="box" onChange={(e) => setEmail(e.target.value)} />
-                    <p>your password <span>*</span></p>
-                    <input type="password" placeholder="enter your password" required maxLength="20" className="box" onChange={(e) => setPassword(e.target.value)} />
-                    <input type="submit" value="login now" className="btn" />
-                    {(isLogedin)? (<div className="alert alert-error">{message}</div>): "" }
-               </form>
-               
+  return (
+    <section className="form-container">
+      <form onSubmit={handleLogin}>
+        <h3>Login Now</h3>
+        <p>Your email <span>*</span></p>
+        <input type="email" placeholder="enter your email" required maxLength="50" className="box" onChange={(e) => setEmail(e.target.value)} />
+        <p>Your password <span>*</span></p>
+        <input type="password" placeholder="enter your password" required maxLength="20" className="box" onChange={(e) => setPassword(e.target.value)} />
+        <input type="submit" value="Login Now" className="btn" />
+        {isLogedin ? (<div className="alert alert-error">{message}</div>) : ""}
+      </form>
+    </section>
+  );
+};
 
-          </section>
-     )
-}
-
-export default UserLogin
+export default UserLogin;
